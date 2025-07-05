5ee123fba9af4e6ade3331b9647c3b206ad89f84
luisdavidpicopita <lpico9694@utm.edu.ec>
2025-07-05 11:40:46 -0500

    SEGUNDO COMMIT

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,29 +2,34 @@ import { Router } from 'express'
 
 const router = Router()
 
-// PÁGINA HOME
+// PÁGINA PRINCIPAL
+
 router.get('/', (req, res) => {
     res.render('index');
 });
 
 // ¿QUÉ ES EL CEC? 
 
-// NATURALEZA E HISTORIA
-
-router.get('/naturaleza_historia', (req, res) => {
-    res.render('naturaleza_historia');
+router.get('/que_es_el_cec', (req, res) => {
+    res.render('que_es_el_cec');
 });
 
-// CAMPAMENTOS
+// PERTENENCIA AL CEC
 
-router.get('/campamentos', (req, res) => {
-    res.render('campamentos');
+router.get('/pertenencia_al_cec', (req, res) => {
+    res.render('pertenencia_al_cec');
 });
 
-// NUESTRA LEY
+// ÁREAS DEL CEC
 
-router.get('/nuestra_ley', (req, res) => {
-    res.render('nuestra_ley');
+router.get('/areas_del_cec', (req, res) => {
+    res.render('areas_del_cec');
+});
+
+// CONTACTO
+
+router.get('/contacto', (req, res) => {
+    res.render('contacto');
 });
 
 
@@ -71,10 +76,12 @@ router.get('/nuestra_ley', (req, res) => {
 
 
 
-router.get('/principal', async (req, res) => {
+
+
+router.get('/a', async (req, res) => {
   try {
     const result = await req.pool.query('SELECT * FROM usuario');
-    res.render('principal', { data: result.rows }); 
+    res.render('a', { data: result.rows }); 
   } catch (error) {
     console.error('Error en consulta:', error);
     res.status(500).send('Error en la base de datos');
